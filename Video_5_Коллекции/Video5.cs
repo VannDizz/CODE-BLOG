9249abbb1a545d167e52a16f75ee346c6d8635f3
Yuriy Klobukov <hoock@ya.ru>
2019-04-02 02:43:38 +0300

    v0.6

--- Video_5_Коллекции/Video5.cs
+++ Video_5_Коллекции/Video5.cs
@@ -12,9 +12,27 @@ namespace Video_5_Коллекции
         {
             //МАССИВЫ ------------------------------------------------
             //Объявление одномерного массива 
-            int[] numbers = { -9, -8, -7 - 6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-
+            int[] numbers = { -9, -8, -7 ,- 6, -5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5,6, 7, 8, 9 };
             
+            // Перебор foreach -------------------------------------------
+            foreach (int i in numbers)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+
+            int n = numbers.Length;
+            int k = n / 2;
+            int temp;
+
+            for (int i = 0; i < k; i++)
+            {
+                temp = numbers[i];
+                numbers[i] = numbers[n - i - 1];
+                numbers[n - i - 1] = temp;
+
+            }
 
 
 
@@ -25,12 +43,12 @@ namespace Video_5_Коллекции
             //    Console.Write(numbers[i]);
             //}
 
-                       
-            // Перебор foreach -------------------------------------------
-            //foreach (int i in numbers)
-            //{
-            //    Console.Write(i + " ");
-            //}
+
+            //Перебор foreach -------------------------------------------
+            foreach (int i in numbers)
+                {
+                    Console.Write(i + " ");
+                }
 
 
 
