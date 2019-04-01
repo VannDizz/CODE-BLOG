11d708919a6fa95af60d41ce9fb19bc9addf550a
Yuriy Klobukov <hoock@ya.ru>
2019-04-01 16:04:28 +0300

    Version 0.2

---
+++ Video_8_ООП/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_8_ООП
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
