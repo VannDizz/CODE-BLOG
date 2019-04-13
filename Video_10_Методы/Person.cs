774a0c31aabf36b230be6e268f188dbcfd3357de
Yuriy Klobukov <hoock@ya.ru>
2019-04-13 11:51:43 +0300

    13_04_19H

---
+++ Video_10_Методы/Person.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_10_Методы
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Person (string firstName, string secondName)
+        {
+            FirstName = firstName;
+            SecondName = secondName;
+
+            X = 0;
+            Y = 0;
+
+        }
+
+        public void Hello()
+        {
+            Console.WriteLine($"Hello {FirstName} {SecondName}");
+        }
+
+        public string Run()
+        {
+            var rnd = new Random();
+            X += rnd.Next(100);
+            Y += rnd.Next(100);
+
+
+            return $"{FirstName} ({X}) ({Y})";
+        }
+
+        public string Run(int x, int y)
+        {
+            X += x;
+            Y += y;
+            return $"{FirstName} ({X}) ({Y})";
+        }
+
+
+
+    }
+}
---
+++ Video_10_Методы/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Video_10_Методы
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Ivan", "Ivanov");
+            Person p2 = new Person("Oleg", "Petrov");
+            p1.Hello();
+            p2.Hello();
+
+            for (var i = 0; i < 10; i++)
+            {
+                var s = p1.Run();
+                Console.WriteLine(s);
+            }
+            Console.ReadKey();
+        }
+
+        public static int PrintHello(string name)
+        {
+            Console.WriteLine($"Привет, {name}!");
+
+
+            return 1;
+        }
+
+    }
+}
